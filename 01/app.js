be80f6c57dfceac59d70347a466ef38fd6dd0e32
DAO VAN SANG <71052657+daovansang@users.noreply.github.com>
2023-04-06 11:19:58 +0900

    01

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,56 @@
 console.log('app.js');
+ //변수
+ var name = 'FE';
+ var age= 28;
+ var message ='hello'+ name;
+
+ //댓글갯수 
+ var commentCount =20;
+ doccument.getElementByTd('wrap').innerHTML = content;
+ //숫자
+ var count = 5;
+ //문자열
+ var nick = "my nick is 'fe'";
+ var homepage = 'www.kakaocorp.com';
+ //불린
+ var isMan = true; 
+ console.log(nick == '');
+ console.log(!!'test');
+ //chuc nang
+ var log = function(str) {
+    console.log(str);
+ };
+ function log(str) {
+    console.log(str);
+ }
+ //su vat
+ var imge ={};
+
+ image.width = 1;
+ image.height = 2;
+
+ image['width']=1;
+ image['name'+1]='1.jpg';
+ image={width:1,heught:2};
+ 
+ //mang
+ image=[];
+
+ image[0]='1.jpg';
+ image[1]='2.jpg';
+
+ image=['1.jpg',2,{name: '3.jpg'}];
+
+ //vo gia tri
+ var nick = null;
+ // khong  xac dinh
+var nick;
+// 주요 연산자
+console.log(1+2);
+console.log('a'+'b');
+console.log('1+4');
+
+
+
+
+ }
