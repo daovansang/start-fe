5b64e3527db022978e1e7120652f8bc853584c8e
DAO VAN SANG <71052657+daovansang@users.noreply.github.com>
2023-04-06 22:18:39 +0900

    chess

---
+++ 06/chess.js
@@ -0,0 +1,37 @@
+function create Board() {
+//const $board = document,creataElement('div);
+// $board -className ='white';
+
+
+    const div = '<div class="$ {color}"></div>';
+    returu div
+}
+
+functinon createRow() {
+
+}
+function createRow(conut = 4,stratcolor = 'white') {
+    let row = '';
+    for (let i = 0; i < conut; i++) {
+        row +=createBoard(startColor);
+
+    }
+    retun row;
+}
+function createChessBoard(count = 4){
+    let board = '';
+}
+board += createRow(4,'white');
+board += createRow(4,'white');
+board += createRow(4,'white');
+board += createRow(4,'white');
+
+// kiem tr
+return board;
+
+$board.addEventlistener('click',Event {
+console.log(Event.target);
+const $target = Event.target;
+});
+$board.innerHTML = createChessBoard();
+
---
+++ 06/drag.js
@@ -0,0 +1,23 @@
+//mousedown
+const $Box = document.getElementById('box');
+let x =0;
+let y =0;
+
+$Box.addEventListener('mousedown', (Event) {
+    console.log('mousedown');
+    console,log(x,y);
+
+    offset.x = $Box.style.left =${x - 50}px';
+    offset.y = $Box.style.left =${y - 50}px';
+
+});
+document.body.addEventListener('mousedown',Event {
+
+});
+document.body.addEventListener('mousedown',Event {
+console.log('mousedown',Event.clieantX,Event.clieantY);
+console.log('mousedown',Event.clieantX,Event.clieantX);
+x =eval.clieantX;
+
+
+
